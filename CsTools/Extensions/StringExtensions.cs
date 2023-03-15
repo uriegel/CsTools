c41d1213461d7acde4a2eddfd017b81358a6e1ff
Uwe Riegel <uriegel@hotmail.de>
2023-03-15 11:54:57 +0100

    Stringextensions: comments WhiteSpaceToNull GetEnvironmentVariable

--- CsTools/Extensions/StringExtensions.cs
+++ CsTools/Extensions/StringExtensions.cs
@@ -4,6 +4,12 @@ namespace CsTools.Extensions;
 
 public static class StringExtensions
 {
+    /// <summary>
+    /// Returns a substring after a found char, not including the char
+    /// </summary>
+    /// <param name="str"></param>
+    /// <param name="startChar"></param>
+    /// <returns></returns>
     public static string SubstringAfter(this string? str, char startChar)
     {
         var posStart = str?.IndexOf(startChar) + 1 ?? -1;
@@ -12,6 +18,12 @@ public static class StringExtensions
         : "";
     }
 
+    /// <summary>
+    /// Returns a substring, until a char in the string is found, not including the found char
+    /// </summary>
+    /// <param name="str"></param>
+    /// <param name="endChar"></param>
+    /// <returns></returns>
     public static string SubstringUntil(this string? str, char endChar)
     {
         var posEnd = str?.IndexOf(endChar) ?? 0;
@@ -20,17 +32,42 @@ public static class StringExtensions
         : str ?? "";
     }
 
+    /// <summary>
+    /// Combination of 'SubstringAfter' and 'SubstringUntil', returning a substring embedded between 'startChar and 'endChar'
+    /// </summary>
+    /// <param name="str"></param>
+    /// <param name="startChar"></param>
+    /// <param name="endChar"></param>
+    /// <returns></returns>
     public static string StringBetween(this string? str, char startChar, char endChar)
         => str
                 ?.SubstringAfter(startChar)
                 ?.SubstringUntil(endChar)
                 ?? "";
 
+    /// <summary>
+    /// Parses a string to get an int value, returning null if parsing not possible
+    /// </summary>
+    /// <param name="str"></param>
+    /// <returns></returns>
     public static int? ParseInt(this string? str)
         => int.TryParse(str, out var val)
             ? val
             : null;
 
+    /// <summary>
+    /// Functional way of calling Stirng.IsNullOrWhiteSpace
+    /// </summary>
+    /// <param name="str"></param>
+    /// <returns></returns>
+    public static string? WhiteSpaceToNull(this string? str)
+        => string.IsNullOrWhiteSpace(str) ? null : str;
+            /// <summary>
+    /// Retrieving an environment variable using this string as the key
+    /// </summary>
+    /// <param name="key"></param>
+    /// <returns></returns>
+    /// <exception cref="Exception"></exception>
     public static string? GetEnvironmentVariable(this string key)
         => ExceptionToNull(() => Environment.GetEnvironmentVariable(key) ?? throw new Exception());
 }            
