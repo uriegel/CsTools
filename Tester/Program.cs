17705366c2ef9e35e4bc64baa1e31b56b1e9e159
Uwe Riegel <uriegel@hotmail.de>
2023-03-26 11:33:55 +0200

    ProgressStream

--- Tester/Program.cs
+++ Tester/Program.cs
@@ -1,4 +1,5 @@
-﻿using CsTools.Extensions;
+﻿using CsTools;
+using CsTools.Extensions;
 
 using static CsTools.ProcessCmd;
 
@@ -9,4 +10,13 @@ stream?.CopyTo(file);
 var result = await RunAsync("lsblkd", "--bytes --output SIZE,NAME,LABEL,MOUNTPOINT,FSTYPE");
 result = await RunAsync("lsblk", "--nothing -bytes -output SIZE,NAME,LABEL,MOUNTPOINT,FSTYPE");
 result = await RunAsync("lsblk", "--bytes --output SIZE,NAME,LABEL,MOUNTPOINT,FSTYPE");
+
+using var testerFile = new ProgressStream(
+    File.OpenRead(Environment.CurrentDirectory.AppendPath(@"Tester/bin/Debug/net6.0/Tester")), 
+    (total, current) => Console.WriteLine($"Total: {total}, current: {current}"));
+using var testerCopy = File.Create(Environment.CurrentDirectory.AppendPath(@"TesterCopy"));
+testerFile.CopyTo(testerCopy);
+
+
+
 Console.ReadLine();
